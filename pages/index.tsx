bf4bbc1d98ea31565ff70161323015dcc4f8e9a1
raikasdev <roni.aikas@gmail.com>
2022-10-05 13:49:37 +0300

    ark

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,18 @@ export default function App({
                       memberObject: i,
                     })),
                     other: defaultOther,
-                    items: [],
+                    items: [{
+                      title: "Työjärjestyksen hyväksyminen",
+                      presention:"Asiat käsitellään asialistan mukaisessa järjestyksessä."
+                    },
+                    {
+                      title: "Kokouksen toteaminen lailliseksi ja päätösvaltaiseksi",
+                      presention: "Kokous todetaan laillisesti koolle kutsutuksi ja päätösvaltaiseksi.\nToimielin on päätösvaltainen, kun paikalla on yli puolet jäsenistä (Nuorisovaltuuston toimintasääntö §5)"
+                    },
+                    {
+                      title: "Valitaan pöytäkirjantarkistajat ja ääntenlaskijat",
+                      presention: "Valitaan kokoukselle pöytäkirjantarkastajat (2kpl). Ääntenlaskijana toimii puheenjohtaja Silja Piirainen."
+                    }],
                     location: defaultLocation,
                   });
                   setPoytakirjat(temp);
@@ -368,6 +379,7 @@ function importData(setPoytakirjat: any, setSelected: any, password: any, setPas
           } else {
             const data = JSON.parse(newestRow.json);
             localStorage.setItem("poytakirjat", newestRow.json);
+            localStorage.setItem("poytakirjat_version", highScore.toString());
             setPoytakirjat(data);
             if (data.length !== 0) {
               setSelected(data[0]);
@@ -377,6 +389,28 @@ function importData(setPoytakirjat: any, setSelected: any, password: any, setPas
         });
     },
   });
+}
+
+function diff(obj1: any, obj2: any) {
+  const result: any = {};
+  if (Object.is(obj1, obj2)) {
+      return undefined;
+  }
+  if (!obj2 || typeof obj2 !== 'object') {
+      return obj2;
+  }
+  Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+      if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+          result[key] = obj2[key];
+      }
+      if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+          const value = diff(obj1[key], obj2[key]);
+          if (value !== undefined) {
+              result[key] = value;
+          }
+      }
+  });
+  return result;
 }
 
 function exportData(password: any, setPassword: any) {
@@ -397,16 +431,34 @@ function exportData(password: any, setPassword: any) {
     labels: { confirm: "Vie ja ylikirjoita", cancel: "Peru" },
     onConfirm: () => {
       supabase
-        .from("data")
-        .insert({
-          json: localStorage.getItem("poytakirjat") || "[]",
-          password: password1
-        })
-        .then((res) => {
-          if (res.error) {
-            alert("Virheellinen salasana")
+        .from<{ json: string; version: number; password: string; }>("data")
+        .select("version,json")
+        .eq('password', password1)
+        .then((rows) => {
+          const versions = rows.body?.map((i) => i.version) || [];
+          var highScore = Math.max.apply(Math, versions); // gives the highest score
+          var scoreIndex = versions.indexOf(highScore); // gives the location of the highest score
+          var newestRow = (rows.body || [])[scoreIndex];
+          if (!newestRow) {
+            alert("Salasana väärin.");
           } else {
-            alert("Vienti onnistui.")
+            const currentVersion = parseInt(localStorage.getItem('poytakirjat_version') || '1000000000');
+            if (highScore > currentVersion) {
+              if(!confirm('Tietokannassa on uudempi versio. Haluatko VARMASTI ylikirjoittaa muutokset?')) return;
+            }
+            supabase
+              .from("data")
+              .insert({
+                json: localStorage.getItem("poytakirjat") || "[]",
+                password: password1
+              })
+              .then((res) => {
+                if (res.error) {
+                  alert("Virheellinen salasana")
+                } else {
+                  alert("Vienti onnistui.")
+                }
+              });
           }
         });
     },
