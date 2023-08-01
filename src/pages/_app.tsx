04c302c5dc4ecdc62bd28001e4c4715dcbaaa53b
raikasdev <roni@mikroni.fi>
2023-08-01 16:34:34 +0300

    Light theme forced

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
-        theme={{ colorScheme }}
+        theme={{ colorScheme: "light" }}
       >
         <Notifications />
         <ModalsProvider>
